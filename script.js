de3f2f550d8dc9feb536a8896be9fe82cf7c02f9
codingmaster675 <topsecretshadowgovernment@gmail.com>
2024-09-01 11:54:42 +0300

    OBJECTS IN JAVASCRIPT

---
+++ script.js
@@ -0,0 +1,14 @@
+// Object is a variable that stores key value pairs
+var student={
+    Name:"Bior",
+    Age:13,
+    Grade:9
+}
+
+var game={
+    Name:"Grand Theft Auto",
+    Genre:"Action-Adventure",
+    Price:"6000Ksh"
+}
+//console.log(game.Name)
+console.log(student.Name)
